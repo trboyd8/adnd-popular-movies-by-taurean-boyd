e0c849c7611e2e22429848a4cc1135255402258e
Taurean Boyd <trboyd8@gmail.com>
2019-08-09 22:10:01 -0400

    Creating the initial recyclerview and related classes.

--- app/src/main/java/com/taureanboyd/popularmovies/MainActivity.java
+++ app/src/main/java/com/taureanboyd/popularmovies/MainActivity.java
@@ -1,13 +1,29 @@
 package com.taureanboyd.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.movieList = findViewById(R.id.rv_movie_list);
+        this.movieList.setLayoutManager(new GridLayoutManager(this, 2));
+
+        List<String> movies = new ArrayList<>();
+        movies.add("Hello");
+        movies.add("World");
+        movies.add("Pineapple");
+        this.movieList.setAdapter(new MovieListAdapter(movies));
     }
 }
---
+++ app/src/main/java/com/taureanboyd/popularmovies/MovieListAdapter.java
@@ -0,0 +1,38 @@
+package com.taureanboyd.popularmovies;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+class MovieListAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+
+    private List<String> movies;
+
+    MovieListAdapter(List<String> movies) {
+        this.movies = movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View movieView = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie, parent, false);
+        return new MovieViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder movieViewHolder, int position) {
+        View movieView = movieViewHolder.itemView;
+        TextView movieContent = movieView.findViewById(R.id.tv_movie);
+        movieContent.setText(this.movies.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+}
